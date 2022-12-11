9cc1ce83bb95ea212b39b9f0513526f71a8a78b1
RomanMRR <roman.mukhametov.r@gmail.com>
2022-12-11 17:44:01 +0300

    Реализован вывод графика загрузки процессора, добавлен функционал изменение времени обновления графика

---
+++ main.py
@@ -0,0 +1,282 @@
+import sys
+import platform
+import os
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime,
+                          QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase,
+                         QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import psutil
+from pyqtgraph import PlotWidget
+import pyqtgraph as pg
+from pathlib import Path
+import numpy as np
+from collections import deque
+import sqlite3
+
+
+# CONSTANTS, time intervals when the chart should be updated
+INTERVAL_1SECOND = 1000
+INTERVAL_10SECOND = 10000
+INTERVAL_1MINUTE = 60000
+
+
+# GLOBALS
+counter = 0
+jumper = 10
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) #enable highdpi scaling
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = uic.loadUi("main.ui", self)
+        self.cpu_percent = 0
+        self.traces = dict()
+        self.timestep = 1
+        self.timestamp = 0
+        self.timeaxis = []
+        self.cpuaxis = []
+        self.ramaxis = []
+        self.current_timer_graph = None
+        self.graph_lim = 15
+        self.deque_timestamp = deque([], maxlen=self.graph_lim+20)
+        self.deque_cpu = deque([], maxlen=self.graph_lim+20)
+
+        self.graphwidget1 = PlotWidget(title="CPU percent")
+        x1_axis = self.graphwidget1.getAxis('bottom')
+        x1_axis.setLabel(text='Time since start (s)')
+        y1_axis = self.graphwidget1.getAxis('left')
+        y1_axis.setLabel(text='Percent')
+
+        self.ui.gridLayout.addWidget(self.graphwidget1, 0, 0, 1, 3)
+
+        self.current_timer_systemStat = QtCore.QTimer()
+        self.current_timer_systemStat.timeout.connect(
+            self.getsystemStatpercent)
+        self.current_timer_systemStat.start(1000)
+        self.show_cpu_graph()
+        self.setting_radioButton()
+
+    def setting_radioButton(self):
+        '''Настраиваем переключатели'''
+        self.radioButton_1Second.clicked.connect(self.change_plot_interval)
+        self.radioButton_10Second.clicked.connect(self.change_plot_interval)
+        self.radioButton_1Minute.clicked.connect(self.change_plot_interval)
+
+    def getsystemStatpercent(self):
+        # gives a single float value
+        self.cpu_percent = psutil.cpu_percent()
+        self.setValue(self.cpu_percent, self.ui.labelPercentageCPU,
+                      self.ui.circularProgressCPU, "rgba(85, 170, 255, 255)")
+
+    def start_cpu_graph(self):
+        if self.current_timer_graph:
+            self.current_timer_graph.stop()
+            self.current_timer_graph.deleteLater()
+            self.current_timer_graph = None
+        self.current_timer_graph = QtCore.QTimer()
+        self.current_timer_graph.timeout.connect(self.update_cpu)
+        self.current_timer_graph.start(1000)
+
+
+    def change_plot_interval(self):
+        '''Выставляем интервал обновления графика'''
+        if self.radioButton_1Second.isChecked():
+            self.current_timer_graph.setInterval(INTERVAL_1SECOND)
+            self.timestep = 1
+        elif self.radioButton_10Second.isChecked():
+            self.current_timer_graph.setInterval(INTERVAL_10SECOND)
+            self.timestep = 10
+        elif self.radioButton_1Minute.isChecked():
+            self.current_timer_graph.setInterval(INTERVAL_1MINUTE)
+            self.timestep = 60
+
+    def update_cpu(self):
+        self.timestamp += self.timestep
+        self.deque_timestamp.append(self.timestamp)
+        self.deque_cpu.append(self.cpu_percent)
+        timeaxis_list = list(self.deque_timestamp)
+        cpu_list = list(self.deque_cpu)
+
+        if self.timestamp > self.graph_lim:
+            self.graphwidget1.setRange(xRange=[self.timestamp-self.graph_lim+1, self.timestamp], yRange=[
+                                       min(cpu_list[-self.graph_lim:]), max(cpu_list[-self.graph_lim:])])
+        self.set_plotdata(name="cpu", data_x=timeaxis_list,
+                          data_y=cpu_list)
+
+
+
+    def show_cpu_graph(self):
+        self.graphwidget1.show()
+        self.start_cpu_graph()
+
+
+
+    def set_plotdata(self, name, data_x, data_y):
+        # print('set_data')
+        if name in self.traces:
+            self.traces[name].setData(data_x, data_y)
+        else:
+            if name == "cpu":
+                self.traces[name] = self.graphwidget1.getPlotItem().plot(
+                    pen=pg.mkPen((85, 170, 255), width=3))
+
+    # ==> SET VALUES TO DEF progressBarValue
+
+    def setValue(self, value, labelPercentage, progressBarName, color):
+
+        sliderValue = value
+
+        # HTML TEXT PERCENTAGE
+        htmlText = """<p align="center"><span style=" font-size:50pt;">{VALUE}</span><span style=" font-size:40pt; vertical-align:super;">%</span></p>"""
+        labelPercentage.setText(htmlText.replace(
+            "{VALUE}", f"{sliderValue:.1f}"))
+
+        # CALL DEF progressBarValue
+        self.progressBarValue(sliderValue, progressBarName, color)
+
+    # DEF PROGRESS BAR VALUE
+    ########################################################################
+
+    def progressBarValue(self, value, widget, color):
+
+        # PROGRESSBAR STYLESHEET BASE
+        styleSheet = """
+        QFrame{
+        	border-radius: 110px;
+        	background-color: qconicalgradient(cx:0.5, cy:0.5, angle:90, stop:{STOP_1} rgba(255, 0, 127, 0), stop:{STOP_2} {COLOR});
+        }
+        """
+
+        # GET PROGRESS BAR VALUE, CONVERT TO FLOAT AND INVERT VALUES
+        # stop works of 1.000 to 0.000
+        progress = (100 - value) / 100.0
+
+        # GET NEW VALUES
+        stop_1 = str(progress - 0.001)
+        stop_2 = str(progress)
+
+        # FIX MAX VALUE
+        if value == 100:
+            stop_1 = "1.000"
+            stop_2 = "1.000"
+
+        # SET VALUES TO NEW STYLESHEET
+        newStylesheet = styleSheet.replace("{STOP_1}", stop_1).replace(
+            "{STOP_2}", stop_2).replace("{COLOR}", color)
+
+        # APPLY STYLESHEET WITH NEW VALUES
+        widget.setStyleSheet(newStylesheet)
+
+
+# ==> SPLASHSCREEN WINDOW
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        # self.ui = Ui_SplashScreen()
+        # self.ui.setupUi(self)
+        self.ui = uic.loadUi("splash_screen.ui", self)
+        # ==> SET INITIAL PROGRESS BAR TO (0) ZERO
+        self.progressBarValue(0)
+
+        # ==> REMOVE STANDARD TITLE BAR
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)  # Remove title bar
+        # Set background to transparent
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # ==> APPLY DROP SHADOW EFFECT
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 120))
+        self.ui.circularBg.setGraphicsEffect(self.shadow)
+
+        # QTIMER ==> START
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        # TIMER IN MILLISECONDS
+        self.timer.start(15)
+
+        # SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+        ## ==> END ##
+
+    # DEF TO LOANDING
+    ########################################################################
+    def progress(self):
+        global counter
+        global jumper
+        value = counter
+
+        # HTML TEXT PERCENTAGE
+        htmlText = """<p><span style=" font-size:68pt;">{VALUE}</span><span style=" font-size:58pt; vertical-align:super;">%</span></p>"""
+
+        # REPLACE VALUE
+        newHtml = htmlText.replace("{VALUE}", str(jumper))
+
+        if(value > jumper):
+            # APPLY NEW PERCENTAGE TEXT
+            self.ui.labelPercentage.setText(newHtml)
+            jumper += 10
+
+        # SET VALUE TO PROGRESS BAR
+        # fix max value error if > than 100
+        if value >= 100:
+            value = 1.000
+        self.progressBarValue(value)
+        if counter == 10:
+            self.main = MainWindow()
+
+        # CLOSE SPLASH SCREE AND OPEN APP
+        if counter > 100:
+            # STOP TIMER
+            self.timer.stop()
+
+            # SHOW MAIN WINDOW
+            # self.main = MainWindow()
+            self.main.show()
+
+            # CLOSE SPLASH SCREEN
+            self.close()
+
+        # INCREASE COUNTER
+        counter += 0.5
+
+    # DEF PROGRESS BAR VALUE
+    ########################################################################
+    def progressBarValue(self, value):
+
+        # PROGRESSBAR STYLESHEET BASE
+        styleSheet = """
+        QFrame{
+        	border-radius: 150px;
+        	background-color: qconicalgradient(cx:0.5, cy:0.5, angle:90, stop:{STOP_1} rgba(255, 0, 127, 0), stop:{STOP_2} rgba(85, 170, 255, 255));
+        }
+        """
+
+        # GET PROGRESS BAR VALUE, CONVERT TO FLOAT AND INVERT VALUES
+        # stop works of 1.000 to 0.000
+        progress = (100 - value) / 100.0
+
+        # GET NEW VALUES
+        stop_1 = str(progress - 0.001)
+        stop_2 = str(progress)
+
+        # SET VALUES TO NEW STYLESHEET
+        newStylesheet = styleSheet.replace(
+            "{STOP_1}", stop_1).replace("{STOP_2}", stop_2)
+
+        # APPLY STYLESHEET WITH NEW VALUES
+        self.ui.circularProgress.setStyleSheet(newStylesheet)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SplashScreen()
+    sys.exit(app.exec_())
